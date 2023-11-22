8fbfe63b9a977e7186f040fe16124297c9bbedff
“Fraser <2653099e@student.gla.ac.uk>
2023-11-22 15:04:55 +0000

    Feat: Implement Navigation On 2d UI (Closes #51)
    * Created Script to show different canvases
    * Created World Object for Script
    * Created Event Manager for Canvases
    * Added On Click Functionality to Nav Buttons

---
+++ CENSIS-AR-App/CENSIS-AR-App/Assets/ExampleAssets/Scripts/CanvasManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour
+{
+    public Canvas[] canvases; // Array to hold your different canvases
+
+    public void ActivateCanvas(int canvasIndex)
+    {
+        // Loop through all canvases
+        for (int i = 0; i < canvases.Length; i++)
+        {
+            // Activate the selected canvas and deactivate others
+            canvases[i].enabled = (i == canvasIndex);
+        }
+    }
+}
