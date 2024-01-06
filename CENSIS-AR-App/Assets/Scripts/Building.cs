cf7fe084989d6eb4f1d71d3cbe3d1d37e378b79e
Joanne <jowelch03@gmail.com>
2024-01-06 18:27:16 +0000

    Feat: Make Building Class (closes #82)
    * Write building class which will be used to store information about
      each of the building in the scavenger hunt

---
+++ CENSIS-AR-App/Assets/Scripts/Building.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Numerics;
+
+[Serializable]
+public class Building
+{
+    public string name;
+    public Vector2[] boundingBox;
+    public string clue;
+    public string information;
+
+    public Building(String name, Vector2[] boundingBox, string clue, string information)
+    {
+        this.name = name;
+        this.boundingBox = boundingBox; 
+        this.clue = clue;
+        this.information = information;
+    }
+}
