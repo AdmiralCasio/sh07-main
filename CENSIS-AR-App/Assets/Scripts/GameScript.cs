5319b317abb687059f805b1033687bd715eceeb5
Joanne <jowelch03@gmail.com>
2024-01-06 17:53:47 +0000

    Feat: Add functions for game play
    * add function which can be called when building is found to display
      data about building and next button
    * add function which is called when next button is pressed which
      displays next clue
    * add clue display function

--- CENSIS-AR-App/Assets/Scripts/GameScript.cs
+++ CENSIS-AR-App/Assets/Scripts/GameScript.cs
@@ -6,12 +6,36 @@ using UnityEngine;
 public class GameScript : MonoBehaviour
 {
     [SerializeField] string filename;
+    int buildingIndex;
     //List<Building> buildings = new List<Building>();
 
     // Start is called before the first frame update
     void Start()
     {
         //buildings = FileHandler.ReadFromJSON<Building>(filename);
+
+        // gets the index of the current building or 0 
+        int buildingIndex = PlayerPrefs.GetInt("CurrentBuilding", 0);
+    }
+
+    public void BuildingFound()
+    {
+        // show info
+        // Debug.Log($"Building name: {buildings[buildingIndex].name}, Building info: {buildings[buildingIndex].information}");
+        // show next button
+    }
+
+    private void Next()
+    {
+        buildingIndex += 1;
+        // show clue
+        ShowClue();
+        // Debug.Log($"Building clue: {buildings[buildingIndex].clue});
+    }
+
+    private void ShowClue()
+    {
+        // show clue;
     }
 
 }
