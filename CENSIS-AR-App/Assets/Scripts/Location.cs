dc4bd21747f7ccfa5f10affa88897bd2d4ae756b
Matthew Lyon <2654239l@student.gla.ac.uk>
2024-01-10 17:36:31 +0000

    Feat: Within Outer Boundary-Within Inner Boundary (Closes #74 #75)
    * Adds Location Validator to give interface commands for determining user location
    * Amends Location.cs to include inner and outer boundary boxes
    * Amends LocationVisbility to simplify the method and make IsVisible public

--- CENSIS-AR-App/Assets/Scripts/Location.cs
+++ CENSIS-AR-App/Assets/Scripts/Location.cs
@@ -1,18 +1,26 @@
 using System;
-using System.Numerics;
+using UnityEngine;
 
 [Serializable]
 public class Location
 {
+    public struct BoundingBox
+    {
+        public Vector2[] inner;
+        public Vector2[] outer;
+    }
+
     public string name;
-    public Vector2[] boundingBox;
+    public BoundingBox boundingBox;
     public string clue;
     public string information;
+    public Vector2 centre;
 
-    public Location(String name, Vector2[] boundingBox, string clue, string information)
+    public Location(String name, Vector2[] inner, Vector2[] outer, string clue, string information)
     {
         this.name = name;
-        this.boundingBox = boundingBox; 
+        this.boundingBox.inner = inner;
+        this.boundingBox.outer = outer;
         this.clue = clue;
         this.information = information;
     }
--- CENSIS-AR-App/Assets/Scripts/LocationVisibility.cs
+++ CENSIS-AR-App/Assets/Scripts/LocationVisibility.cs
@@ -2,13 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LocationVisibility 
+public class LocationVisibility
 {
-    static bool IsVisible(Vector3 target,Camera camera){
-        Vector3 screenPoint=camera.WorldToViewportPoint(target);
-        if((screenPoint.x>0 && screenPoint.x<1)&&(screenPoint.y>0 && screenPoint.y<1) &&(screenPoint.z>0)){
-            return true;
-        }
-        return false;
-        }
+    public static bool IsVisible(Vector3 target, Camera camera)
+    {
+        Vector3 screenPoint = camera.WorldToViewportPoint(target);
+        return screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1 && screenPoint.z > 0;
     }
+}
