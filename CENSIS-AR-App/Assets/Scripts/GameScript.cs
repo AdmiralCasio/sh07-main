41cebcf079e238ef5ac9033cf4fbd1806ad55f10
Joanne <jowelch03@gmail.com>
2024-01-15 19:36:22 +0000

    Feat: Added UI elements and updated script
    * added next and show clue buttons
    * added clue overlay
    * updated script to reference these

--- CENSIS-AR-App/Assets/Scripts/GameScript.cs
+++ CENSIS-AR-App/Assets/Scripts/GameScript.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameScript : MonoBehaviour
 {
     [SerializeField] string filename;
+    Canvas clue = GameObject.Find("ClueOverlay").GetComponent<Canvas>();
+    Button nextButton = GameObject.Find("NextButton").GetComponent<Button>();
+    Button showClue = GameObject.Find("ShowClue").GetComponent<Button>();
+
 
 
     // Start is called before the first frame update
@@ -14,6 +19,9 @@ public class GameScript : MonoBehaviour
         // get locations from file
         List<Location> locations = FileHandler.ReadFromJSON<Location>(filename);
         LocationHandler.locations = locations;
+        clue.enabled = false;
+        nextButton.enabled = false;
+        
     }
 
     public void LocationFound()
@@ -21,15 +29,21 @@ public class GameScript : MonoBehaviour
         // show info
         Debug.Log($"Location name: {LocationHandler.GetCurrLocation().name}, Building info: {LocationHandler.GetCurrLocation().information}");
         // show next button
+        nextButton.enabled = true;
+        showClue.enabled = false;
+
+
     }
 
-    private void Next()
+    public void Next()
     {
         // switch to next location
         if (LocationHandler.UpdateLocation())  {
             // show clue
             ShowClue();
             Debug.Log($"Locations clue: {LocationHandler.GetCurrLocation().clue}");
+            nextButton.enabled = false;
+            showClue.enabled = true;
         }
         else
         {
@@ -37,9 +51,16 @@ public class GameScript : MonoBehaviour
         }
     }
 
-    private void ShowClue()
+    public void ShowClue()
     {
         // show clue;
+        clue.enabled = true;
+    }
+
+    public void CloseClue()
+    {
+        // close clue
+        clue.enabled = false;
     }
 
     void GameWon()
