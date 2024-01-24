abe27b55ef2f155ab2f248b3c0091bd544c3bab0
Ahmed Ibrahim <2653288i@student.gla.ac.uk>
2024-01-24 15:52:51 +0000

    Fix : fixed location and relevant classes
    * changes how locations are represented in location.cs
    * changed how to check if at location

--- CENSIS-AR-App/Assets/Scripts/Location.cs
+++ CENSIS-AR-App/Assets/Scripts/Location.cs
@@ -6,23 +6,51 @@ public class Location
 {
     public struct BoundingBox
     {
-        public Vector2[] inner;
-        public Vector2[] outer;
+        public Vector2[] points;    
     }
 
+    public BoundingBox[] inner;
+    public BoundingBox[] outer;
     public string name;
-    public BoundingBox boundingBox;
     public string clue;
     public string information;
     public Vector2 centre;
 
-    public Location(String name, Vector2[] inner, Vector2[] outer, string clue, string information)
+    public Location(String name,string clue, string information, float [][][] inner, float[][][] outer)
     {
         this.name = name;
-        this.boundingBox.inner = inner;
-        this.boundingBox.outer = outer;
+        this.inner = new BoundingBox[inner.Length];
+        
+        for (int i = 0; i < inner.Length; i++)
+        {
+            //Debug.Log("LOCATION INNER length: " + inner.Length);
+            //Debug.Log("LOCATION INNER i: " +  i);
+            Vector2[] temp = new Vector2[inner[i].Length];
+            for (int j = 0; j < inner[i].Length; j++)
+            {
+                //Debug.Log("LOCATION INNER j: " + j);
+                //Debug.Log("LOCATION INNER  i length: " + inner[i].Length);
+                //Debug.Log("LOCATION INNER j[]: " + inner[i][j][0] + " , " + inner[i][j][1]);
+                temp[j] = new Vector2(inner[i][j][0], inner[i][j][1]);
+                //this.inner[i].points[j] = new Vector2(inner[i][j][0], inner[i][j][1]);
+            }
+            this.inner[i].points = temp;
+        }
+        
+
+        this.outer = new BoundingBox[outer.Length];
+        for (int i = 0; i < outer.Length; i++)
+        {
+            Vector2[] temp = new Vector2[outer[i].Length];
+            for (int j = 0; j < outer[i].Length; j++)
+            {
+                temp[j] = new Vector2(outer[i][j][0], outer[i][j][1]);
+            }
+            this.outer[i].points = temp;
+        }
         this.clue = clue;
         this.information = information;
         this.centre = centre;
+
     }
 }
--- CENSIS-AR-App/Assets/Scripts/LocationValidator.cs
+++ CENSIS-AR-App/Assets/Scripts/LocationValidator.cs
@@ -19,7 +19,25 @@ public class LocationValidator
 
     public static bool AtLocation(Vector2 position,  Location location)
     {
-        return InBox(position, location.boundingBox.outer) && !InBox(position, location.boundingBox.inner);
+        bool inInner = false;
+        bool inOuter = false;
+
+        foreach (var box in location.outer) {
+            int boxNumber = 0;
+            if (InBox(position, location.outer[boxNumber].points)){
+                inOuter = true;
+            }
+        }
+
+        foreach (var box in location.inner)
+        {
+            int boxNumber = 0;
+            if (InBox(position, location.inner[boxNumber].points)){
+                inInner = true;
+            }
+        }
+
+        return inOuter && !inInner;
     }
 
     public static bool LookingAtLocation(Vector2 position, Location location)
