12fe48629e56c773025c9e6210d48677d71fa482
Matthew Lyon <2654239l@student.gla.ac.uk>
2024-01-27 14:29:43 +0000

    Fix: Image Running
    * Added code to build image prior to using dotnet tools

--- ci/lint.sh
+++ ci/lint.sh
@@ -3,9 +3,10 @@
 set -e
 set -x
 
-echo "Linting using $image in $PWD"
+echo "Linting using $IMAGE_NAME in $PWD"
+docker build -t monoapp .
+docker run -it --rm monoapp
 dotnet new tool-manifest
 dotnet tool install csharpier
 dotnet tool restore
 dotnet csharpier --check
-
