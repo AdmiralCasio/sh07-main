26eec922de982376f8da09ec8ebd80e15630c96a
Matthew Lyon <2654239l@student.gla.ac.uk>
2024-01-29 14:54:36 +0000

    Revert "Feat: Lint Step"
    This reverts commit 02be36b638faa9b1df68346a1354c1b4e43b5af2.

--- ci/docker_lint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-docker run \
-  -w /project/CENSIS-AR-App/Assets/Scripts \
-  -v $UNITY_DIR:/project/ \
-  $IMAGE_NAME \
-  /bin/bash -c "/project/ci/lint.sh && /project/ci/lint.sh"
