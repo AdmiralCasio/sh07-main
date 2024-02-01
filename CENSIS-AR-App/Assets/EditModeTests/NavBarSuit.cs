4537e621d38351ed204410961cd48f3d1e88758f
Joanne <2659539w@student.gla.ac.uk>
2024-02-01 13:30:23 +0000

    Fix: Remove redundant test and change app name
     * remove redundant test which was failing
     * change file name to not include a space

--- CENSIS-AR-App/Assets/EditModeTests/NavBarSuit.cs
+++ CENSIS-AR-App/Assets/EditModeTests/NavBarSuit.cs
@@ -21,20 +21,6 @@ public class NavBarSuit : InputTestFixture
             return GameObject.Find(canvas).GetComponent<Canvas>();
         }
 
-        [UnityTest]
-        public IEnumerator TestPageOnStartup()
-        {
-            yield return null;
-            // check home canvas is enabled
-            Assert.IsTrue(GetCanvas("HomeCanvas").enabled);
-
-            // check other canvases are not enabled
-            Assert.IsFalse(GetCanvas("CameraCanvas").enabled);
-            Assert.IsFalse(GetCanvas("MapCanvas").enabled);
-            Assert.IsFalse(GetCanvas("InfoCanvas").enabled);
-
-            yield return null;
-        }
 
         [UnityTest]
         public IEnumerator TestHomeButton()
