c5c57479ffdd2d3346864b179ae94980fee59efb
Matthew Lyon <2654239l@student.gla.ac.uk>
2024-02-05 23:54:14 +0000

    Feat: Home Screen Map
    * Added code to control map size on home screen
    * Added movement script to map

--- CENSIS-AR-App/Assets/Scripts/CanvasManager.cs
+++ CENSIS-AR-App/Assets/Scripts/CanvasManager.cs
@@ -1,3 +1,6 @@
+using Mapbox.Map;
+using Mapbox.Unity.Map;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,6 +8,8 @@ using UnityEngine.UI;
 public class CanvasManager : MonoBehaviour
 {
     public Canvas[] canvases; // Array to hold your different canvases
+    public Camera mapCamera;
+    public AbstractMap map;
 
     public void ActivateCanvas(int canvasIndex)
     {
@@ -13,6 +18,20 @@ public class CanvasManager : MonoBehaviour
         {
             // Activate the selected canvas and deactivate others
             canvases[i].enabled = (i == canvasIndex);
+        }
+
+        int[] mapCanvases = new int[] { 0, 1, 3 };
+
+        mapCamera.gameObject.SetActive(mapCanvases.Contains(canvasIndex));
+        map.gameObject.SetActive(mapCanvases.Contains(canvasIndex));
+
+        if (canvasIndex == 0)
+        {
+            mapCamera.rect = new Rect(mapCamera.rect.x, mapCamera.rect.y, mapCamera.rect.width, 0.4f);
+        }
+        else
+        {
+            mapCamera.rect = new Rect(mapCamera.rect.x, mapCamera.rect.y, mapCamera.rect.width, 1);
         }
     }
     void Awake()
