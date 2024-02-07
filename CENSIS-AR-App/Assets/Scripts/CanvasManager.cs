1f23c50c59f9af63559519625462e9a80cbf8eb5
Matthew Lyon <2654239l@student.gla.ac.uk>
2024-02-07 16:31:35 +0000

    Feat: Map (Closes #108)
    * Implemented map features
    * Dragging, panning
    * User location
    * Centering
    * Fixes map extent bug
    * Manages map switching between canvases
    * Home screen map

--- CENSIS-AR-App/Assets/Scripts/CanvasManager.cs
+++ CENSIS-AR-App/Assets/Scripts/CanvasManager.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.UI;
-
+using Mapbox.Unity.Map;
+using System.Linq;
 
 public class CanvasManager : MonoBehaviour
 {
     public Canvas[] canvases; // Array to hold your different canvases
-
+    public AbstractMap map;
+    public Camera mapCamera;
     public void ActivateCanvas(int canvasIndex)
     {
         // Loop through all canvases
@@ -14,9 +16,26 @@ public class CanvasManager : MonoBehaviour
             // Activate the selected canvas and deactivate others
             canvases[i].enabled = (i == canvasIndex);
         }
+
+        int[] activeCanvases = { 0, 1, 3 };
+        mapCamera.gameObject.SetActive(activeCanvases.Contains(canvasIndex));
+        map.gameObject.SetActive(activeCanvases.Contains(canvasIndex));
+
+        if (canvasIndex == 0)
+        {
+            mapCamera.rect = new Rect(mapCamera.rect.x, mapCamera.rect.y,  mapCamera.rect.width, 0.4f);
+            map.UpdateMap();
+        }
+
+        else
+        {
+            mapCamera.rect = new Rect(mapCamera.rect.x, mapCamera.rect.y, mapCamera.rect.width, 1);
+            map.UpdateMap();
+        }
     }
-    void Awake()
-    {
+    void Start()
+    { 
+
         ActivateCanvas(1);
         ActivateCanvas(0);
     }
