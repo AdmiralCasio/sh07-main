d0b0b314fc198a259fc38a2bc2ae1f2534704f93
Ahmed Ibrahim <2653288i@student.gla.ac.uk>
2024-02-14 14:41:27 +0000

    Feat: Started Location Adder form
    * created form with text input for name, clue, information, and url for open street map location

--- Utils/FindBuildingPolygon.py
+++ Utils/FindBuildingPolygon.py
@@ -6,6 +6,7 @@ sys.tracebacklimit = 100
 
 
 def extractNodesFromWay(WayID):
+    api = Api()
     way = api.query("way/{}".format(WayID))
     wayNodes = []
     for i in way.nodes():
@@ -13,33 +14,32 @@ def extractNodesFromWay(WayID):
         wayNodes.append(node)
     return wayNodes
 
-
-try:
-    api = Api()
-    building = sys.argv[1]
-    form = building.split('/')[-2]
-    nodes = []
-    if form == "relation":
-        relation = api.query("/".join(building.split('/')[-2:]))
-        ways = []
-        for i in relation.members():
-            ways.append(i.id())
-        waysInRelation = relation.members()[0].id()
-        for i in ways:
-            nodes.append(extractNodesFromWay(i))
-        nodes = list(chain.from_iterable(nodes))
-    elif form == "way":
-        nodes = extractNodesFromWay(building.split('/')[-1])
-    else:
-        print("Please enter a Way or Relation link")
-
-    out = "[ \n [ \n "
-    for i in nodes:
-        out += (str(i) + '\n')
-    out = out[:-3] if out[-3] == ',' else out
-    out += "\n]\n]"
-    with open("Utils/nodes.txt", 'w') as f:
-        f.write(out)
-
-except Exception as error:
-    print(type(error).__name__, error)
+def getVectors(url):
+    try:
+        api = Api()
+        building = url
+        form = building.split('/')[-2]
+        nodes = []
+        if form == "relation":
+            relation = api.query("/".join(building.split('/')[-2:]))
+            ways = []
+            for i in relation.members():
+                ways.append(i.id())
+            waysInRelation = relation.members()[0].id()
+            for i in ways:
+                nodes.append(extractNodesFromWay(i))
+            nodes = list(chain.from_iterable(nodes))
+        elif form == "way":
+            nodes = extractNodesFromWay(building.split('/')[-1])
+        else:
+            print("Please enter a Way or Relation link")
+    
+        out = "[ \n [ \n "
+        for i in nodes:
+            out += (str(i) + '\n')
+        out = out[:-3] if out[-3] == ',' else out
+        out += "\n]\n]"
+        return out
+    
+    except Exception as error:
+        print(type(error).__name__, error)
---
+++ Utils/LocationAdder.py
@@ -0,0 +1,47 @@
+﻿import tkinter as tk
+from tkinter import *
+from FindBuildingPolygon import *
+
+window = tk.Tk()
+
+
+def read():
+    title = (str(buildingEntry.get()))
+    clue = (str(clueEntry.get()))
+    info = (str(infoEntry.get()))
+    url = (str(osmUrlEntry.get()))
+    print(getVectors(url))
+
+window.title("Add Location")
+
+# Buildine Name
+buildingLabel = tk.Label(window, text="Building Name")
+buildingLabel.grid(row=0, column=0)
+
+buildingEntry = tk.Entry(window)
+buildingEntry.grid(row=0, column=1)
+
+# Clue
+clueLabel = tk.Label(window, text="Clue")
+clueLabel.grid(row=1, column=0)
+
+clueEntry = tk.Entry(window)
+clueEntry.grid(row=1, column=1)
+
+# Info
+infoLabel = tk.Label(window, text="Information")
+infoLabel.grid(row=2, column=0)
+
+infoEntry = tk.Entry(window)
+infoEntry.grid(row=2, column=1)
+
+# Building URL
+osmUrlLabel = tk.Label(window, text="OSM Location link")
+osmUrlLabel.grid(row=3,column=0)
+
+osmUrlEntry = tk.Entry(window)
+osmUrlEntry.grid(row=3,column=1)
+
+button = tk.Button(window, text="submit", command=read).grid(row=4, column=1)
+
+window.mainloop()
